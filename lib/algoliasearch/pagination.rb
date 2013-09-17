4cc6bcf9538a68ee080eaa38bfb3b0c0dfb041a9
Sylvain UTARD <sylvain.utard@gmail.com>
2013-09-17 21:45:50 +0200

    No pagination backend by default

--- lib/algoliasearch/pagination.rb
+++ lib/algoliasearch/pagination.rb
@@ -5,6 +5,7 @@ module AlgoliaSearch
     autoload :Kaminari, 'algoliasearch/pagination/kaminari'
 
     def self.create(results, total_hits, options = {})
+      return results if AlgoliaSearch.configuration[:pagination_backend].nil?
       begin
         backend = AlgoliaSearch.configuration[:pagination_backend].to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase } # classify pagination backend name
         page = Object.const_get(:AlgoliaSearch).const_get(:Pagination).const_get(backend).create(results, total_hits, options)
