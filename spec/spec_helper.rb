6d1bbb345e066aa097abd1c7651a0fe1f37f1205
Sylvain UTARD <sylvain.utard@gmail.com>
2013-09-17 20:17:55 +0200

    Do not fail silently if environment variables are missing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'algoliasearch-rails'
 require 'rspec'
 
+raise "missing ALGOLIA_APPLICATION_ID or ALGOLIA_API_KEY environment variables" if ENV['ALGOLIA_APPLICATION_ID'].nil? || ENV['ALGOLIA_API_KEY'].nil?
+
 AlgoliaSearch.configuration = { application_id: ENV['ALGOLIA_APPLICATION_ID'], api_key: ENV['ALGOLIA_API_KEY'] }
 
 RSpec.configure do |c|
