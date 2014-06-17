8c01784412bbaa4bb7896bc3e2344a2c3301c020
Sylvain UTARD <sylvain.utard@gmail.com>
2014-06-17 18:31:27 +0200

    Kaminari: I finally got you!! They changed the behavior of PaginableArray in 0.16.0... try to handle both

--- lib/algoliasearch/pagination/kaminari.rb
+++ lib/algoliasearch/pagination/kaminari.rb
@@ -25,8 +25,14 @@ module AlgoliaSearch
       class << self
         def create(results, total_hits, options = {})
           offset = ((options[:page] - 1) * options[:per_page])
-          results = ([nil] * offset) + results # pad results with nil values so it matches the offset param
-          new results, :offset => offset, :limit => options[:per_page], :total_count => total_hits
+          array = new results, :offset => offset, :limit => options[:per_page], :total_count => total_hits
+          if array.empty? and !results.empty?
+            # since Kaminari 0.16.0, you need to pad the results with nil values so it matches the offset param
+            # otherwise you'll get an empty array: https://github.com/amatsuda/kaminari/commit/29fdcfa8865f2021f710adaedb41b7a7b081e34d
+            results = ([nil] * offset) + results
+            array = new results, :offset => offset, :limit => options[:per_page], :total_count => total_hits
+          end
+          array
         end
       end
     end
