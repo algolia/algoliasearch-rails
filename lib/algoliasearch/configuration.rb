895705f336cdc6ab7fbe91e56ec79ebf4681025e
Sylvain UTARD <sylvain.utard@gmail.com>
2014-07-10 18:45:39 +0200

    Pass the whole configuration hash to the underlying Algolia.init method

--- lib/algoliasearch/configuration.rb
+++ lib/algoliasearch/configuration.rb
@@ -6,7 +6,7 @@ module AlgoliaSearch
 
     def configuration=(configuration)
       @@configuration = configuration
-      Algolia.init :application_id => @@configuration[:application_id], :api_key => @@configuration[:api_key], :hosts => @@configuration[:hosts]
+      Algolia.init @@configuration
     end
   end
 end
