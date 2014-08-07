32d9dd744609fe2a14920b8aff7d7c2eab5f370c
thomas morgan <tm@iprog.com>
2014-08-06 23:25:48 -0600

    Fixes searches on Mongoid as broken by commit 003cdec.
    The API for where() when querying for multiple IDs differs between ActiveRecord
    and Mongoid.
      ActiveRecord: where(:id => [...])
      Mongoid:      where(:id.in => [...])
    This patch detects Mongoid and adjust queries accordingly.

--- lib/algoliasearch-rails.rb
+++ lib/algoliasearch-rails.rb
@@ -357,7 +357,12 @@ module AlgoliaSearch
       end
       json = algolia_raw_search(q, params)
       hit_ids = json['hits'].map { |hit| hit['objectID'] }
-      results_by_id = algoliasearch_options[:type].where(algolia_object_id_method => hit_ids).index_by do |hit|
+      if defined?(::Mongoid::Document) && self.include?(::Mongoid::Document)
+        condition_key = algolia_object_id_method.in
+      else
+        condition_key = algolia_object_id_method
+      end
+      results_by_id = algoliasearch_options[:type].where(condition_key => hit_ids).index_by do |hit|
         algolia_object_id_of(hit)
       end
       results = json['hits'].map do |hit|
