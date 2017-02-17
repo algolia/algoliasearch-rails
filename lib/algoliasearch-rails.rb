e35a36351046859dedeb2c569b885011ef71edc8
Sylvain UTARD <sylvain.utard@gmail.com>
2017-02-17 10:21:36 +0100

    Fixed the handling of `raise_on_failture`.
    We set an instance variable but used a class variable...
    
    Fix #207

--- lib/algoliasearch-rails.rb
+++ lib/algoliasearch-rails.rb
@@ -288,7 +288,7 @@ module AlgoliaSearch
 
     ::Algolia::Index.instance_methods(false).each do |m|
       define_method(m) do |*args, &block|
-        SafeIndex.log_or_throw(m) do
+        SafeIndex.log_or_throw(m, @raise_on_failure) do
           @index.send(m, *args, &block)
         end
       end
@@ -297,14 +297,14 @@ module AlgoliaSearch
     # special handling of wait_task to handle null task_id
     def wait_task(task_id)
       return if task_id.nil? && !@raise_on_failure # ok
-      SafeIndex.log_or_throw(:wait_task) do
+      SafeIndex.log_or_throw(:wait_task, @raise_on_failure) do
         @index.wait_task(task_id)
       end
     end
 
     # special handling of get_settings to avoid raising errors on 404
     def get_settings(*args)
-      SafeIndex.log_or_throw(:get_settings) do
+      SafeIndex.log_or_throw(:get_settings, @raise_on_failure) do
         begin
           @index.get_settings(*args)
         rescue Algolia::AlgoliaError => e
@@ -316,17 +316,17 @@ module AlgoliaSearch
 
     # expose move as well
     def self.move_index(old_name, new_name)
-      SafeIndex.log_or_throw(:move_index) do
+      SafeIndex.log_or_throw(:move_index, true) do
         ::Algolia.move_index(old_name, new_name)
       end
     end
 
     private
-    def self.log_or_throw(method, &block)
+    def self.log_or_throw(method, raise_on_failure, &block)
       begin
         yield
       rescue Algolia::AlgoliaError => e
-        raise e if @raise_on_failure
+        raise e if raise_on_failure
         # log the error
         (Rails.logger || Logger.new(STDOUT)).error("[algoliasearch-rails] #{e.message}")
         # return something
