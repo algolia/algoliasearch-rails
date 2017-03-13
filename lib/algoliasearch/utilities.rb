f1582d868926b5e685641e3cb4ce97aaf37b787a
Sylvain UTARD <sylvain.utard@gmail.com>
2017-03-13 10:32:55 +0100

    Make sure get_model_classes is not ActiveRecord compliant only

--- lib/algoliasearch/utilities.rb
+++ lib/algoliasearch/utilities.rb
@@ -2,9 +2,8 @@ module AlgoliaSearch
   module Utilities
     class << self
       def get_model_classes
-        Rails.application.eager_load! # Ensure all models are loaded (not necessary in production when cache_classes is true).
-
-        ActiveRecord::Base.descendants.select{ |model| model.respond_to?(:algolia_reindex) }
+        Rails.application.eager_load! if Rails.application # Ensure all models are loaded (not necessary in production when cache_classes is true).
+        AlgoliaSearch.instance_variable_get :@included_in
       end
 
       def clear_all_indexes
