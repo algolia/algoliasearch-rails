5f70cb4566bb2adceb2e54549c7e663d9ce88910
Devin Beeuwkes <46448173+DevinCodes@users.noreply.github.com>
2022-03-10 09:17:21 +0100

    fix: only check current settings when indexing enabled (#416)
    * fix: only check current settings when indexing enabled
    
    * chore: add tests
    
    * chore: add test
    
    * fix: ensure index exists
    
    * remove sleep
    
    * Update algoliasearch-rails.rb
    
    * Update integration_spec.rb

--- lib/algoliasearch-rails.rb
+++ lib/algoliasearch-rails.rb
@@ -533,6 +533,7 @@ module AlgoliaSearch
         # fetch the master settings
         master_index = algolia_ensure_init(options, settings)
         master_settings = master_index.get_settings rescue {} # if master doesn't exist yet
+        master_exists = master_settings != {}
         master_settings.merge!(JSON.parse(settings.to_settings.to_json)) # convert symbols to strings
 
         # remove the replicas of the temporary index
@@ -546,7 +547,7 @@ module AlgoliaSearch
         tmp_options.delete(:per_environment) # already included in the temporary index_name
         tmp_settings = settings.dup
 
-        if options[:check_settings] == false
+        if options[:check_settings] == false && master_exists
           AlgoliaSearch.client.copy_index!(src_index_name, tmp_index_name, { scope: %w[settings synonyms rules] })
           tmp_index = SafeIndex.new(tmp_index_name, !!options[:raise_on_failure])
         else
@@ -791,7 +792,7 @@ module AlgoliaSearch
 
       options[:check_settings] = true if options[:check_settings].nil?
 
-      current_settings = if options[:check_settings]
+      current_settings = if options[:check_settings] && !algolia_indexing_disabled?(options)
                            @algolia_indexes[settings].get_settings(:getVersion => 1) rescue nil # if the index doesn't exist
                          end
 
