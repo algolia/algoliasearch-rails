5788edae2dea36a0691deed96a02b5ac0695d6f3
Devin Beeuwkes <46448173+DevinCodes@users.noreply.github.com>
2024-10-21 13:38:01 +0200

    feat: upgrade to newest API client version (#449)
    * chore: update algolia version
    
    * WIP: fix tests until FowardToReplicas
    
    * chore: fix SubReplicas tests
    
    * chore: fix Book tests
    
    * chore: fix pagination tests
    
    * chore: fix disabled indexing tests
    
    * fix tests
    
    * ensure latest gem version
    
    * fix: virtual replica test
    
    * test matrix
    
    * Update Gemfile
    
    * Update config.yml
    
    * Update Gemfile
    
    * Update config.yml
    
    * Update config.yml
    
    * Update algoliasearch-rails.rb
    
    * sanitizer
    
    * Update algoliasearch-rails.rb
    
    * Update algoliasearch-rails.rb
    
    * Update algoliasearch-rails.rb
    
    * Update pagy.rb
    
    * Update integration_spec.rb
    
    * Update pagy.rb
    
    * Update integration_spec.rb
    
    * Update pagy.rb
    
    * Update pagy.rb
    
    * Update integration_spec.rb
    
    * Update integration_spec.rb
    
    * sanitizer
    
    * chore: clean up algoliasearch/configuration
    
    * remove `index` method
    
    * chore: handle todos
    
    * Update algoliasearch-rails.gemspec
    
    * Update spec_helper.rb
    
    * address feedback
    
    * feat: allow setting user agent
    
    * pin version
    
    * Update algoliasearch-rails.gemspec
    
    * Update config.yml
    
    * prep release
    
    * feat: issue sync

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,8 +32,12 @@ RSpec.configure do |c|
   # Remove all indexes setup in this run in local or CI
   c.after(:suite) do
     safe_index_list.each do |i|
-      index = AlgoliaSearch.client.init_index(i['name'])
-      index.delete!
+      begin
+        res = AlgoliaSearch.client.delete_index(i.name)
+        AlgoliaSearch.client.wait_for_task(i.name, res.task_id)
+      rescue
+        # fail gracefully
+      end
     end
   end
 end
@@ -48,7 +52,7 @@ end
 
 # get a list of safe indexes in local or CI
 def safe_index_list
-  list = AlgoliaSearch.client.list_indexes['items']
-  list = list.select { |index| index["name"].include?(SAFE_INDEX_PREFIX) }
-  list.sort_by { |index| index["primary"] || "" }
+  list = AlgoliaSearch.client.list_indices.items
+  list = list.select { |index| index.name.include?(SAFE_INDEX_PREFIX) }
+  list.sort_by { |index| index.primary || "" }
 end
